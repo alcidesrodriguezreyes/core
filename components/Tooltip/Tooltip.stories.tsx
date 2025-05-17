0a50fa925e905436d4ff44d777d43a4e5cb36e68
Alcides Rodriguez <alcidesrodriguezreyes@gmail.com>
2025-05-17 13:45:46 -0500

    Add Storybook stories for Tooltip component

---
+++ components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Tooltip from "./index";
+
+const meta: Meta<typeof Tooltip> = {
+    title: "Tooltip",
+    component: Tooltip,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+    args: {
+        content: "Tooltip",
+    }
+};
+
+export const Large: Story = {
+    args: {
+        content: "Tooltip",
+        large: true,
+    },
+};
